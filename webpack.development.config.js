915e9c516bfa2e1298d31c4f32c6d26325b5f966
Donal Stewart <covidresponse@demonsoft.org>
2021-03-14 16:20:43 +0000

    Replace create-react-app tooling with Webpack 5

---
+++ webpack.development.config.js
@@ -0,0 +1,152 @@
+"use strict";
+process.env.BABEL_ENV = "development";
+process.env.NODE_ENV = "development";
+
+const path = require("path");
+const webpack = require("webpack");
+const resolve = require("resolve");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+const WatchMissingNodeModulesPlugin = require("react-dev-utils/WatchMissingNodeModulesPlugin");
+
+const cwd = process.cwd();
+
+module.exports = {
+  context: cwd,
+  mode: "development",
+  devtool: "eval-cheap-module-source-map",
+  entry: path.resolve("src/index.js"),
+  output: {
+    path: path.resolve("devbuild"),
+    filename: "static/js/bundle.js",
+    publicPath: "/",
+    globalObject: "this",
+  },
+  resolve: {
+    extensions: ["js", "json", "jsx"].map((ext) => `.${ext}`),
+  },
+  module: {
+    rules: [
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+          {
+            test: /\.(js|mjs|jsx)$/,
+            include: path.resolve("src"),
+            loader: require.resolve("babel-loader"),
+            options: {
+              customize: require.resolve(
+                "babel-preset-react-app/webpack-overrides"
+              ),
+              plugins: [
+                [
+                  require.resolve("babel-plugin-named-asset-import"),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent:
+                          "@svgr/webpack?-svgo,+titleProp,+ref![path]",
+                      },
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: false,
+            },
+          },
+          {
+            test: /\.(js|mjs)$/,
+            exclude: /@babel(?:\/|\\{1,2})runtime/,
+            loader: require.resolve("babel-loader"),
+            options: {
+              babelrc: false,
+              configFile: false,
+              compact: false,
+              presets: [
+                [
+                  require.resolve("babel-preset-react-app/dependencies"),
+                  { helpers: true },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              sourceMaps: true,
+              inputSourceMap: true,
+            },
+          },
+          {
+            test: /\.css$/,
+            use: [
+              require.resolve("style-loader"),
+              {
+                loader: require.resolve("css-loader"),
+                options: { importLoaders: 1, sourceMap: false },
+              },
+            ],
+            sideEffects: true,
+          },
+          {
+            test: /\.(csv|tsv)$/,
+            use: ["raw-loader"],
+          },
+          // Default loader - should be last
+          {
+            loader: require.resolve("file-loader"),
+            // Exclude `js` files to keep "css" loader working as it injects
+            // its runtime that would otherwise be processed through "file" loader.
+            // Also exclude `html` and `json` extensions so they get processed
+            // by webpacks internal loaders.
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: { name: "static/media/[name].[hash:8].[ext]" },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(cwd, "public"),
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new ESLintPlugin({ extensions: ["js", "mjs", "jsx"] }),
+    // Generates an `index.html` file with the <script> injected.
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve("public/index.html"),
+    }),
+    // Makes some environment variables available in index.html.
+    // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+    // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
+    // It will be an empty string unless you specify "homepage"
+    // in `package.json`, in which case it will be the pathname of that URL.
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
+      NODE_ENV: "development",
+      PUBLIC_URL: ".",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development"),
+        PUBLIC_URL: JSON.stringify("."),
+      },
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new WatchMissingNodeModulesPlugin(path.resolve("node_modules")),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment$/,
+    }),
+  ],
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,239 @@
+"use strict";
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
+
+const path = require("path");
+const webpack = require("webpack");
+const resolve = require("resolve");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const InlineChunkHtmlPlugin = require("react-dev-utils/InlineChunkHtmlPlugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+// Variable used for enabling profiling in Production
+// passed into alias object. Uses a flag if passed into the build command
+const isEnvProductionProfile = process.argv.includes("--profile");
+const publicUrl = "./";
+
+const cwd = process.cwd();
+
+module.exports = {
+  mode: "production",
+  context: cwd,
+  bail: true,
+  devtool: "source-map",
+  entry: [path.resolve("src/index.js")],
+  output: {
+    path: path.resolve("build"),
+    filename: "static/js/[name].[contenthash:8].js",
+    chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+    publicPath: publicUrl,
+    devtoolModuleFilenameTemplate: (info) =>
+      path
+        .relative(path.resolve("src"), info.absoluteResourcePath)
+        .replace(/\\/g, "/"),
+    // this defaults to 'window', but by setting it to 'this' then
+    // module chunks which are built will work in web workers as well.
+    // globalObject: "this",
+    clean: true,
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: { ecma: 8 },
+          compress: { comparisons: false, inline: 2 },
+          mangle: { safari10: true },
+          // Added for profiling in devtools
+          keep_classnames: isEnvProductionProfile,
+          keep_fnames: isEnvProductionProfile,
+          format: { comments: false, ascii_only: true },
+        },
+      }),
+      new CssMinimizerPlugin({
+        sourceMap: true,
+        minimizerOptions: {
+          preset: ["default", { minifyFontValues: { removeQuotes: false } }],
+        },
+      }),
+    ],
+    splitChunks: { chunks: "all", name: false },
+    runtimeChunk: { name: (entrypoint) => `runtime-${entrypoint.name}` },
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: ["js", "json", "jsx"].map((ext) => `.${ext}`),
+    alias: {
+      // Allows for better profiling with ReactDevTools
+      ...(isEnvProductionProfile && {
+        "react-dom$": "react-dom/profiling",
+        "scheduler/tracing": "scheduler/tracing-profiling",
+      }),
+    },
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      // Disable require.ensure as it's not a standard language feature.
+      { parser: { requireEnsure: false } },
+      {
+        oneOf: [
+          {
+            test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+            loader: require.resolve("url-loader"),
+            options: {
+              limit: 10000,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+          // Process application JS with Babel.
+          // The preset includes JSX, Flow, TypeScript, and some ESnext features.
+          {
+            test: /\.(js|mjs|jsx|ts|tsx)$/,
+            include: path.resolve("src"),
+            loader: require.resolve("babel-loader"),
+            options: {
+              customize: require.resolve(
+                "babel-preset-react-app/webpack-overrides"
+              ),
+              plugins: [
+                [
+                  require.resolve("babel-plugin-named-asset-import"),
+                  {
+                    loaderMap: {
+                      svg: {
+                        ReactComponent:
+                          "@svgr/webpack?-svgo,+titleProp,+ref![path]",
+                      },
+                    },
+                  },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              compact: true,
+            },
+          },
+          // Process any JS outside of the app with Babel.
+          // Unlike the application JS, we only compile the standard ES features.
+          {
+            test: /\.(js|mjs)$/,
+            exclude: /@babel(?:\/|\\{1,2})runtime/,
+            loader: require.resolve("babel-loader"),
+            options: {
+              babelrc: false,
+              configFile: false,
+              compact: false,
+              presets: [
+                [
+                  require.resolve("babel-preset-react-app/dependencies"),
+                  { helpers: true },
+                ],
+              ],
+              cacheDirectory: true,
+              cacheCompression: false,
+              sourceMaps: true,
+              inputSourceMap: true,
+            },
+          },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                // css is located in `static/css`, use '../../' to locate index.html folder
+                options: publicUrl.startsWith(".")
+                  ? { publicPath: "../../" }
+                  : {},
+              },
+              {
+                loader: require.resolve("css-loader"),
+                options: { importLoaders: 1, sourceMap: true },
+              },
+            ],
+            sideEffects: true,
+          },
+          {
+            test: /\.(csv|tsv)$/,
+            use: ["raw-loader"],
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new ESLintPlugin({ extensions: ["js", "mjs", "jsx"] }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve("public/index.html"),
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+
+    new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime-.+[.]js/]),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, {
+      NODE_ENV: "production",
+      PUBLIC_URL: publicUrl.slice(0, -1),
+    }),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        PUBLIC_URL: JSON.stringify(publicUrl.slice(0, -1)),
+      },
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash:8].css",
+      chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+    }),
+    new WebpackManifestPlugin({
+      fileName: "asset-manifest.json",
+      publicPath: publicUrl,
+      generate: (seed, files, entrypoints) => {
+        const manifestFiles = files.reduce((manifest, file) => {
+          manifest[file.name] = file.path;
+          return manifest;
+        }, seed);
+        const entrypointFiles = entrypoints.main.filter(
+          (fileName) => !fileName.endsWith(".map")
+        );
+        return { files: manifestFiles, entrypoints: entrypointFiles };
+      },
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new WorkboxWebpackPlugin.GenerateSW({
+      clientsClaim: true,
+      exclude: [/\.map$/, /asset-manifest\.json$/],
+      navigateFallback: publicUrl + "index.html",
+      navigateFallbackDenylist: [
+        new RegExp("^/_"),
+        new RegExp("/[^/?]+\\.[^/]+$"),
+      ],
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("public"),
+          to: path.resolve("build"),
+          globOptions: { ignore: [path.resolve("public/index.html")] },
+        },
+      ],
+    }),
+  ],
+};
